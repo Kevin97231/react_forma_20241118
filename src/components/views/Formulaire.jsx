ab56fe75f0e7b89a09b39471ddc9bc6c54be6ee8
klabarre <klabarre@dawan.fr>
2024-11-19 14:24:47 +0100

    ajout composant formulaire.jsx

--- src/components/views/Formulaire.jsx
+++ src/components/views/Formulaire.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
+/* eslint-disable react/no-unescaped-entities */
 function Formulaire() {
-  return <h1 className="pt-20">Les formulaires</h1>;
+  const [name, setName] = useState("");
+
+  function handleNameChanged(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <>
+      <h1 className="pt-20">Les formulaires</h1>
+      <h2>Les champs contrôlés</h2>
+      <p>
+        En react, nous avons un concept que nous pouvons appeler les 'champs
+        contrôlés'. En utilisant le <strong> 'onChange'</strong> d'une input et
+        le hook useState() nous pouvons mettre à jour une variable à chaque
+        changement de la valeur présente dans l'input
+      </p>
+
+      <div className="p-5 mt-5 border w-fit">
+        <p>Bonjour, {name}</p>
+        <input
+          className="input-bordered"
+          type="text"
+          value={name}
+          onChange={handleNameChanged}
+        />
+      </div>
+    </>
+  );
 }
 
 export default Formulaire;
