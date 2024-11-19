e72d45d2743dab3f444f26f5af6d065ed992d64a
klabarre <klabarre@dawan.fr>
2024-11-19 12:10:40 +0100

    push fin de matinée

--- src/components/views/Flux.jsx
+++ src/components/views/Flux.jsx
@@ -1,5 +1,13 @@
 function Flux() {
-  return <h1 className="pt-20">Flux</h1>;
+  const data = [
+    { id: 1, nom: "Produit 1", prix: 20.99, quantite: 5 },
+    { id: 2, nom: "Produit 2", prix: 15.45, quantite: 10 },
+    { id: 3, nom: "Produit 3", prix: 75.32, quantite: 58 },
+    { id: 4, nom: "Produit 4", prix: 30.0, quantite: 25 },
+    { id: 5, nom: "Produit 5", prix: 26.0, quantite: 16 },
+  ];
+
+  return <h1 className="pt-20">les flux de données</h1>;
 }
 
 export default Flux;
