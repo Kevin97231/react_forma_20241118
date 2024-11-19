2efead769db9fda69662592ac860133413d5a7f0
klabarre <klabarre@dawan.fr>
2024-11-19 14:47:20 +0100

    ajout ocnsigne xo

--- src/components/views/Exercice.jsx
+++ src/components/views/Exercice.jsx
@@ -1,5 +1,12 @@
+import Consigne from "../Consigne";
+
 function Exercice() {
-  return <h1 className="pt-20">Exercice !!!</h1>;
+  return (
+    <>
+      <h1 className="pt-20">Exercice !!!</h1>
+      <Consigne />
+    </>
+  );
 }
 
 export default Exercice;
