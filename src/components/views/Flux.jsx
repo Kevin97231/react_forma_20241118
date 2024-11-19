598a9f590bcd32881e1ff7e90f26b33c4f1ad14f
klabarre <klabarre@dawan.fr>
2024-11-19 13:47:03 +0100

    début d'AM

--- src/components/views/Flux.jsx
+++ src/components/views/Flux.jsx
@@ -7,7 +7,32 @@ function Flux() {
     { id: 5, nom: "Produit 5", prix: 26.0, quantite: 16 },
   ];
 
-  return <h1 className="pt-20">les flux de données</h1>;
+  return (
+    <>
+      <h1 className="pt-20">les flux de données</h1>;
+      <h2>De parents vers enfants</h2>
+      <p>
+        Pour faire passer des données d'un composant parent vers le composant
+        enfant, nous devons utiliser les 'props'
+      </p>
+      <ComposantEnfant data={data} />
+      <h2>De l'enfant vers le parent</h2>
+      <p>
+        En react, pour faire passer des données d'un composant enfant vers son
+        parent, il est courant d'utiliser des fonctions de rappels (callbacks)
+      </p>
+    </>
+  );
 }
+
+const ComposantEnfant = ({ data }) => {
+  return data.map((produit) => (
+    <div key={produit.id}>
+      <p>nom : {produit.nom}</p>
+      <p>prix : {produit.prix}</p>
+      <p>quantité : {produit.quantite}</p>
+    </div>
+  ));
+};
 
 export default Flux;
