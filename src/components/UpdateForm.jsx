e8de581244ce948b213bfdd1a9a7a069561c83ee
klabarre <klabarre@dawan.fr>
2024-11-21 15:31:13 +0100

    ajout exemple biblio react hook form

--- src/components/UpdateForm.jsx
+++ src/components/UpdateForm.jsx
@@ -1,77 +1,112 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 export const UpdateForm = ({ productToModify, updateProduct, closeModal }) => {
   const { id } = productToModify;
 
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [number, setNumber] = useState("");
-  const [category, setCategory] = useState("");
+  //  Logique sans la bibliotheque react hook form
+  // const [name, setName] = useState("");
+  // const [price, setPrice] = useState("");
+  // const [number, setNumber] = useState("");
+  // const [category, setCategory] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    clearErrors,
+  } = useForm();
 
   useEffect(() => {
-    setName(productToModify.name || "");
-    setPrice(productToModify.price || "");
-    setNumber(productToModify.number || "");
-    setCategory(productToModify.category || "");
+    clearErrors();
+    setValue("name", productToModify.name || "");
+    setValue("price", productToModify.price || "");
+    setValue("number", productToModify.number || "");
+    setValue("category", productToModify.category || "");
+    // TODO: Pré remplissage avec le setValue
+    // Sans la biblio React hook form
+    // setName(productToModify.name || "");
+    // setPrice(productToModify.price || "");
+    // setNumber(productToModify.number || "");
+    // setCategory(productToModify.category || "");
   }, [productToModify]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    switch (name) {
-      case "name":
-        setName(value);
-        break;
-      case "price":
-        setPrice(value);
-        break;
-      case "number":
-        setNumber(value);
-        break;
-      case "category":
-        setCategory(value);
-        break;
-    }
+  // //  Logique sans la bibliotheque react hook form
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   switch (name) {
+  //     case "name":
+  //       setName(value);
+  //       break;
+  //     case "price":
+  //       setPrice(value);
+  //       break;
+  //     case "number":
+  //       setNumber(value);
+  //       break;
+  //     case "category":
+  //       setCategory(value);
+  //       break;
+  //   }
+  // };
+
+  //  Logique sans la bibliotheque react hook form
+  // const submit = (e) => {
+  //   console.log("FORMULAIRE SOUMIS !");
+  //   e.preventDefault();
+  //   updateProduct({ name, price, number, category, id });
+  //   closeModal();
+  // };
+
+  const submit = (product) => {
+    console.log(product);
+    updateProduct({ ...product, id });
+    closeModal();
   };
 
-  const submit = (e) => {
-    e.preventDefault();
-    updateProduct({ name, price, number, category, id });
-    closeModal();
+  let numberVerif = {
+    required: "valeur requise",
+    min: { value: 0, message: "Valeur min: 0" },
+    max: { value: 100, message: "Valeur max: 0" },
   };
 
   return (
     <section>
-      <form onSubmit={submit}>
+      <form onSubmit={handleSubmit(submit)}>
         <div className="grid grid-cols-4 mb-5 gap-5">
           <input
             type="text"
-            value={name}
-            onChange={handleChange}
-            name="name"
             className="grow"
+            {...register("name", {
+              required: "Nom requis",
+            })}
           />
+          {errors.name && <span>{errors.name.message}</span>}
+
           <input
             type="number"
-            value={price}
-            name="price"
             className="grow"
-            onChange={handleChange}
+            {...register("price", numberVerif)}
           />
+          {errors.price && <span>{errors.price.message}</span>}
+
           <input
             type="number"
-            value={number}
-            name="number"
             className="grow"
-            onChange={handleChange}
+            {...register("number", numberVerif)}
           />
+          {errors.number && <span>{errors.number.message}</span>}
           <input
             type="text"
-            value={category}
-            name="category"
             className="grow"
-            onChange={handleChange}
+            {...register("category", {
+              required: "catégorie requise",
+              maxLength: { value: 10, message: "10 caractères max !" },
+            })}
           />
+          {errors.category && <span>{errors.category.message}</span>}
         </div>
         <button type="submit" className="btn">
           Valider
@@ -80,3 +115,44 @@ export const UpdateForm = ({ productToModify, updateProduct, closeModal }) => {
     </section>
   );
 };
+
+// Sans biblio react hook form:
+//  return (
+//    <section>
+//      <form onSubmit={submit}>
+//        <div className="grid grid-cols-4 mb-5 gap-5">
+//          <input
+//            type="text"
+//            value={name}
+//            onChange={handleChange}
+//            name="name"
+//            className="grow"
+//          />
+//          <input
+//            type="number"
+//            value={price}
+//            name="price"
+//            className="grow"
+//            onChange={handleChange}
+//          />
+//          <input
+//            type="number"
+//            value={number}
+//            name="number"
+//            className="grow"
+//            onChange={handleChange}
+//          />
+//          <input
+//            type="text"
+//            value={category}
+//            name="category"
+//            className="grow"
+//            onChange={handleChange}
+//          />
+//        </div>
+//        <button type="submit" className="btn">
+//          Valider
+//        </button>
+//      </form>
+//    </section>
+//  );
