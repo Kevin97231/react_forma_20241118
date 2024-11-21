f3b48e2812779e7ce81d3f3d097ac3e5142a74cc
klabarre <klabarre@dawan.fr>
2024-11-21 14:44:20 +0100

    ajout exemple useMemo

--- src/components/WithUseMemo.jsx
+++ src/components/WithUseMemo.jsx
@@ -1,3 +1,34 @@
+import { useMemo, useState } from "react";
+
 export const WithUseMemo = () => {
-  return <>{/* Contenu de votre composant */}</>;
+  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
+  const [inputValue, setInputValue] = useState("");
+
+  const sum = useMemo(() => {
+    console.log("Calcul avec useMemo");
+    return numbers.reduce((total, currentValue) => total + currentValue);
+  }, [numbers]);
+
+  const addNumbers = () => {
+    setNumbers((prev) => [...prev, Math.random() * 10]);
+  };
+
+  return (
+    <section className="m-5">
+      <h2>Composant avec useMemo</h2>
+      <p>Somme: {sum}</p>
+      <button className="btn" onClick={addNumbers}>
+        Ajouter un chiffre
+      </button>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          className="mt-2 input input-bordered"
+        />
+        {inputValue}
+      </div>
+    </section>
+  );
 };
--- src/components/WithoutUseMemo.jsx
+++ src/components/WithoutUseMemo.jsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+
 export const WithoutUseMemo = () => {
-  return <>{/* Contenu de votre composant */}</>;
+  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
+  const [inputValue, setInputValue] = useState("");
+
+  const sum = numbers.reduce((total, currentValue) => total + currentValue);
+
+  console.log("Chargement du composant qui n'utilise pas 'useMemo' ");
+
+  const addNumbers = () => {
+    setNumbers((prev) => [...prev, Math.random() * 10]);
+  };
+
+  return (
+    <section className="m-5">
+      <h2>Composant sans useMemo</h2>
+      <p>Somme: {sum}</p>
+      <button className="btn" onClick={addNumbers}>
+        Ajouter un chiffre
+      </button>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          className="mt-2 input input-bordered"
+        />
+        {inputValue}
+      </div>
+    </section>
+  );
 };
